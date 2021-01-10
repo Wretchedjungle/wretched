72eba12338b746993b5e50af9befe24821f44ce9
Wretchedjungle <61056341+Wretchedjungle@users.noreply.github.com>
2021-01-10 14:00:46 +1000

    Update index.py

--- index.py
+++ index.py
@@ -2,38 +2,61 @@ import random
 import time
 import requests
 
-print(f"Status Updater (*private client*)")
-print(f" ")
-
-cookie = "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_965406218B1EC097B21F6148C0E8DB3B8B131131B210A0A449823DDEAF70A2621A6EFEE14FA279D9B1D7E35106BBC18F1B428B59020C417C6A908514358CDCC8D516806D5FB1D740090A276E8BE381A54E92E670DCA08631A0FC6874666E92F66EEED846DCD19EFBCC6931B43DBA6ACE61540778B4A62FA8BCE4425C4D23037406B4BCED0ECA5E21B20D888F54F0743FE678F72EEE95C8059395141D97814A7FEDBA3F5DFC7C7D42E9BEA69138FCC0C7291933F9E29BC81190C179D759D3725CE3B2BE1BAC88F9CA6414DBE683715A528C71FB2E5F8214ECFDDFED6E08A86FDF020F45FC5A5FAA4560DD8713DA05C35258963EB1E29884B9B64C944884B8B4B37EF7DBE70C5B0E8B0F4D8A43D97445F1EC2307B6BE6CFC7CCD8B7F49897A3EEE8FCEC9D1E6BCA39E4D0A6B4EE0046D7D3EEA91D4"
+print(f"Follow bot v2.0 (PRIVATE CLIENT)")
+print(f"Made by Wretched#0002")
+time.sleep(3)
 
 session = requests.session()
+print(" ")
+cookie = "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_9602EC009B0FFC42A0A92B44487E70D1A086C1178D9D776F3761BA8B99143B535AB5F4739211D23591E05B14C31EA7925E0488F56886363C094EFEB2FEEEBDD318C8500073F97B6BF636187EC59DAC80BAB09E270285F2116A4991BACD3428BFA120F3F1669408F8947030BE9C494B3BF44DF520C21DE90614E7C0ECDA9ECCD63801A0A5DB6EBE9E6C67BCF15A662815E19C670DFCBD5FCA7709EBCDEB7C91553FA98494A556E8C6D79D74455B9DF4C9568B6B56A60BEC70914430F1FBD162AF2874D86CAA52E0D3FE8E384DE8DABD01B306FF166D21E06A25907AF3F3C94CBE832EB00A036B1F0B4509353F11A27777874251DFDDAA55977709706E4547921AE2755F6618DD4E81FF58D3967F517B9A2BB665BF25EFEB7393ABA036913E8FA43A8D31DBB2A21CC3122974D85A27A7974DF1DF28"
 session.cookies['.ROBLOSECURITY'] = cookie
 
 def grabCSRF():
-   r = session.post("https://auth.roblox.com/v2/user/passwords/change")
-   csrf = r.headers['x-csrf-token']
-   return csrf
-
+       r = session.post("https://auth.roblox.com/v2/user/passwords/change")
+       csrf = r.headers['x-csrf-token']
+       return csrf
+       
 token = grabCSRF()
 r = session.get('https://api.roblox.com/users/account-info', headers={'x-csrf-token':token})
 UserId = r.json()['UserId']
 Username = r.json()['Username']
 r = requests.get(f"https://friends.roblox.com/v1/users/{UserId}/followings/count")
 followingcount = r.json().pop("count")
-print(f" ")
+time.sleep(1)
+print(" ")
 print(f"Successfully logged into {Username} │ User ID {UserId} │ Following {followingcount} users")
 print(f" ")
 
-def update():
-       q = requests.get("https://friends.roblox.com/v1/users/179334070/followings/count")
-       json = q.json()
-       followings = json.pop("count")
+def getFollowings():
+       ffff = requests.get(f"https://friends.roblox.com/v1/users/{UserId}/followings/count")
+       ffffjson = ffff.json()
+       count = ffffjson.pop("count")
+       return count
 
-       rrr = session.patch("https://users.roblox.com/v1/users/179334070/status", headers={'x-csrf-token': token, 'status': f'Following {followings} users.'})
-       rjso = rrr.json()
-       print(f"Response | {rjso}")
+def follow():
+       followedcount = 0
+       while True:
+              try:
+                     csrf = grabCSRF()
+                     targetid = random.randint(2000000000, 2060000000)
+                     r = session.post(f"https://api.roblox.com/user/follow?followedUserId={targetid}" , headers={'x-csrf-token':csrf})
+                     nextcursor = r.json()
+                     checkifbanned = session.get(f"https://users.roblox.com/v1/users/{targetid}")
+                     isbannedjson = checkifbanned.json()
+                     targetname = isbannedjson["name"]
+                     isbanned = isbannedjson["isBanned"]
+                     successmsg = nextcursor["message"]
+                     if isbanned == False:
+                            if successmsg == "Success":
+                                   followedcount += 1
+                                   print(f"Successfully followed {targetname}. {followedcount} users were followed so far.")
+                            else:
+                                   print(f"Account is on cooldown. Cooling down for 15 minutes. You now follow {getFollowings()} users.")
+                                   time.sleep(900)
+                                   print(f" ")
+                     else:
+                            print(f"{targetname} is banned, failed to follow.")
+              except Exception:
+                     pass
 
-while True:
-       update()
-       time.sleep(120)
+follow()
